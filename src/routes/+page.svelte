3e17e32dfb97c30138656eccecb7dd087fb3a561
Shuflduf <shuflduf@shuflduf.xyz>
2025-06-04 13:58:04 -0600

    feat: added little shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,8 @@
 </script>
 
 <main class="container">
-  <img src="/gup.png" alt="Gup" class="fixed -z-10 w-full h-full shadow-lg" />
+  <img src="/gup.png" alt="Gup" class="fixed -z-10 w-full h-full" />
+  <img src="/gup.png" alt="Gup" class="fixed -z-20 w-full h-1/2 bottom-0 brightness-0 opacity-30 scale-x-90" />
   <form data-tauri-drag-region onsubmit={submit} class="flex flex-col items-center gap-4 h-screen justify-center px-16">
     <input
       placeholder="What do you need?"
