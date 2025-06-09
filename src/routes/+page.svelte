3fe6982f7d62cdb76750b0f2b8eec0d4bc54cc44
Shuflduf <shuflduf@shuflduf.xyz>
2025-06-09 16:57:20 -0600

    feat: put it in a box above gup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,18 @@
   import { onMount } from "svelte";
   import { animate } from "animejs";
   import { moveWindow, Position } from "@tauri-apps/plugin-positioner";
+    import { marked } from "marked";
 
   let prompt = $state("");
   let response = $state("");
-  let pos = $state({ x: -400, y: 40, h: 1.0, w: 1.0, r: 0.0 });
+  let responseHTML = $state("");
+  let pos = $state({ x: -400, y: 60, h: 1.0, w: 1.0, r: 0.0 });
 
   async function submit(event: Event) {
     event.preventDefault();
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     response = await invoke("prompt_gemini", { prompt });
+    responseHTML = await marked.parse(response);
   }
 
   onMount(() => {
@@ -27,7 +30,7 @@
       ],
       y: [
         { to: 200, ease: "outQuad", duration: 400 },
-        { to: 40, ease: "inQuad", duration: 400 },
+        { to: 60, ease: "inQuad", duration: 400 },
       ],
       h: [
         { to: 1.4, duration: 0 },
@@ -38,9 +41,8 @@
         { to: 1.0, ease: "outQuad", duration: 600 },
       ],
       r: [
-        { to: 20, duration: 0 },
-        { to: -20, ease: "outQuad", duration: 200 },
-        { to: 0, ease: "inQuad", duration: 400 },
+        { to: -20, duration: 0 },
+        { to: 0, ease: "inQuad", duration: 600 },
       ],
       onUpdate: () => {
         console.log("Position updated:", pos);
@@ -50,11 +52,12 @@
 </script>
 
 <div class="h-[600px] fixed w-[400px] border"></div>
+<div class="fixed h-[250px] w-full bg-yellow-200/70 p-4 rounded-lg prose overflow-y-auto scrollbar-hidden backdrop-blur-md">{@html responseHTML}</div>
 <section
   class="fixed h-[300px] w-[400px]"
   style="right: {pos.x}px; bottom: {pos.y}px;"
 >
-  <div class="relative left-0 z-30 text-white flex flex-row gap-4">
+  <div class="relative left-10 top-10 z-30 text-white flex flex-row gap-4">
     <h1 data-tauri-drag-region>M</h1>
     <button onclick={reset}>R</button>
   </div>
@@ -66,7 +69,7 @@
   />
   <form
     onsubmit={submit}
-    class="flex flex-col items-center gap-4 h-full justify-center px-12 absolute z-20 top-0 w-full"
+    class="flex flex-col items-center gap-4 h-full justify-center px-12 absolute z-20 top-12 w-full"
   >
     <input
       placeholder="What do you need?"
@@ -74,6 +77,16 @@
       class="p-2 px-4 bg-yellow-200/70 rounded-lg outline-none backdrop-blur-xs"
       onsubmit={submit}
     />
-    <p class="text-white h-30 break-words overflow-y-auto">{response}</p>
+    <!-- <p class="text-white h-30 break-words overflow-y-auto">{response}</p> -->
   </form>
 </section>
+
+<style>
+  .scrollbar-hidden {
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
+  .scrollbar-hidden::-webkit-scrollbar {
+    display: none;
+  }
+</style>
